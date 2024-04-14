026a29418a6a707d1d86da257d76218091f12a7e
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2024-04-14 11:25:56 -0500

    updated task.js

--- api/task.js
+++ api/task.js
@@ -8,60 +8,68 @@ import {
 } from "@aws-sdk/lib-dynamodb";
 import crypto from "crypto";
 
-const client = new DynamoDBClient({ region: "us-west-1" });
+const client = new DynamoDBClient({ region: "us-east-1" });
 const docClient = DynamoDBDocumentClient.from(client); 
 
 export const fetchTasks = async () => {
     const command = new ScanCommand({
-        ExpressionAttributeNames: { "#name": "name" },
-        ProjectionExpression: "id, #name, completed",
-        TableName: "Tasks",
+      ExpressionAttributeNames: { "#name": "name" },
+      ProjectionExpression: "id, #name, completed",
+      TableName: "Tasks",
     });
+  
     const response = await docClient.send(command);
-   return response; 
-};
-
-export const createTasks = async ({ name , completed }) => {
-    const uuid = crypto.randomUUID()
+  
+    return response;
+  };
+  
+  export const createTasks = async ({ name, completed }) => {
+    const uuid = crypto.randomUUID();
     const command = new PutCommand({
-        TableName: "Tasks",
-        Item: {
-            id: uuid,
-            name, 
-            completed,
-        },
+      TableName: "Tasks",
+      Item: {
+        id: uuid,
+        name,
+        completed,
+      },
     });
+  
     const response = await docClient.send(command);
-    return response;  
-};
-
-export const updateTasks = async ({ id, name, completed }) => {
+  
+    return response;
+  };
+  
+  export const updateTasks = async ({ id, name, completed }) => {
     const command = new UpdateCommand({
-        TableName: "Tasks",
-        Key: {
-            id,
-        },
-        ExpressionAttributeNames: {
-            "#name": "name",
-        },
-        UpdateExpression: "set #name = :n, completed + :c",
-        ExpressionAttributeValues: {
-            ":n": name, 
-            "c": completed,
-        },
-        ReturnValues: "ALL_NEW",
+      TableName: "Tasks",
+      Key: {
+        id,
+      },
+      ExpressionAttributeNames: {
+        "#name": "name",
+      },
+      UpdateExpression: "set #name = :n, completed = :c",
+      ExpressionAttributeValues: {
+        ":n": name,
+        ":c": completed,
+      },
+      ReturnValues: "ALL_NEW",
     });
+  
     const response = await docClient.send(command);
-    return response;  
-};
-
-export const deleteTasks = async (id) => {
+  
+    return response;
+  };
+  
+  export const deleteTasks = async (id) => {
     const command = new DeleteCommand({
-       TableName: "Tasks",
-       Key: {
-        id, 
-       },
+      TableName: "Tasks",
+      Key: {
+        id,
+      },
     });
+  
     const response = await docClient.send(command);
-    return response;  
-};
+  
+    return response;
+  };
