1e18c286208227561dfc19e6d60068a8bf9d90ac
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2024-04-17 18:12:31 -0500

    updated utils

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,2 +1,2 @@
 
-export const API_URL = "https://xgrtmkdtx2ws55uy36tzum3fza0oacih.lambda-url.us-east-1.on.aws/task";
+export const API_URL = "https://xgrtmkdtx2ws55uy36tzum3fza0oacih.lambda-url.us-east-1.on.aws/Tasks";
